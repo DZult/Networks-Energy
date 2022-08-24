9c704377dbce3a8ffbe163dfa633565870380f9a
DZult <dzulty@mail.ru>
2022-08-24 21:15:14 +0600

    added main page layout

--- src/app/components/content/content.component.ts
+++ src/app/components/content/content.component.ts
@@ -1,4 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import {MoviesService} from "../../services/movies.service";
+
+interface Preload {
+  Title: string;
+  Year: string;
+  Type: string;
+  imdbID: string;
+  Poster: string;
+}
+
+interface Movie {
+  title: string,
+  year: string,
+  type: string,
+  imdbID: string,
+  poster: string,
+  description: string,
+}
 
 @Component({
   selector: 'app-content',
@@ -6,10 +24,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./content.component.scss']
 })
 export class ContentComponent implements OnInit {
+  public moviesList: Movie[] = [];
+  public swiperPages: number[] = [1, 2, 3, 4];
 
-  constructor() { }
+  constructor(private moviesService: MoviesService) { }
 
   ngOnInit(): void {
+    this.moviesService.getMoviesList().subscribe(res => {
+      this.moviesList = res.Search.slice(0, 4).map((item: Preload) => {
+        return {
+          title: item.Title,
+          year: item.Year,
+          type: item.Type,
+          imdbID: item.imdbID,
+          poster: item.Poster,
+          description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut nemo neque sapiente. Doloremque enim eveniet necessitatibus, quam repellendus sed ullam?",
+        }
+      });
+      console.log(this.moviesList)
+    })
   }
 
 }
